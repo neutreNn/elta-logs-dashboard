ed40e59968453f19f63e1f5c66edba7ad5b73d43
neutreNn <vanya-kozlov-1991@mail.ru>
2025-08-07 12:15:41 +0300

    Исправил проблему с смещением времени на +3 часа

--- frontend/src/utils/formatDate.js
+++ frontend/src/utils/formatDate.js
@@ -1,12 +1,14 @@
 const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    return new Intl.DateTimeFormat('ru-RU', {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
-    }).format(date);
-  };
-  
-  export default formatDate;
+  if (!dateString) return 'Нет данных';
+  const date = new Date(dateString);
+  return new Intl.DateTimeFormat('ru-RU', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'UTC'
+  }).format(date);
+};
+
+export default formatDate;
